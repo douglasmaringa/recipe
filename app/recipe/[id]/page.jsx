7b7a3709c4479d507cdee4cf2b20e547b5230232
kevinxinming <86008249+kevinxinming@users.noreply.github.com>
2024-08-28 16:49:31 -0400

    hsi

--- app/recipe/[id]/page.jsx
+++ app/recipe/[id]/page.jsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 
+'use server'
 async function getData(id) {
   const res = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
   if (!res.ok) {
